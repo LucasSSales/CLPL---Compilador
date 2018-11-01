3aec71c8892c049fa2f10a8444c8f18b2ee20e9b
CarlosW1998 <carlos-walte@hotmail.com>
2018-10-31 23:00:45 -0300

    Ações de redução

--- Analisador_sintatico/Analisador_Sintatico.py
+++ Analisador_sintatico/Analisador_Sintatico.py
@@ -1,6 +1,10 @@
-class AnalisadorSintatico():
-    def __init__(self):
+from Analisador_Lexico.Analizador_Lexico import *
+class SlrParse():
+    def __init__(self, filename):
         self.table = self.getTable()
+        self.actions = self.getActions()
+        self.stack = []
+        self.myreader = reader(filename)
     def getTable(self):
         op = open("SLRTable.csv", 'r').read()
         op = op.split('\n')
@@ -17,3 +21,10 @@ class AnalisadorSintatico():
             for j in table.keys():
                 table[j].append(i.pop(0))
         return table
+    def getActions(self):
+        op = open("SLRTable.csv", 'r').read()
+        op = op.split('\n')
+        t = []
+        for i in op:
+            t.append(i.split(" "))
+        return  t
